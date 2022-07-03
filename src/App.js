5a3b18266545baee3f96ae5786487e4c49082007
Oladitan Saliu <86123461+saliu-oladitan@users.noreply.github.com>
2022-07-03 11:43:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited <code>src/App.js</code> and save to reload. 
+          Edited aws<code>src/App.js</code> and save to reload. 
         </p>
         <a
           className="App-link"
